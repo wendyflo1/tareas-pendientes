c68f3851f4b0c36fa1b02f3d62716c0e49c8a871
Wendowski <91354643+wendyflo1@users.noreply.github.com>
2024-08-09 02:01:27 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,92 @@
-//console.log("Hola mundo! desde la consola");
-var app = document.getElementById('app');
+//''
+//definir las constantes
 
-var typewriter = new Typewriter(app, {
-    loop: true
+const fecha = document.querySelector('#fecha');
+const lista = document.querySelector('#lista');
+const elemento = document.querySelector('#elemento');
+const tarea = document.querySelector('#tarea');
+const input = document.querySelector('#input');
+const botonAgregar = document.querySelector('#botonAgregar');
+const check = 'task_alt';
+const tachado = 'tachado';
+const uncheck = 'radio_button_unchecked';
+let LIST; 
+let ID;
+
+const FECHA = new Date ();
+fecha.innerHTML = FECHA.toLocaleDateString ('es-MX', {weekday:'long',month:'long', day:'numeric'});
+
+
+// Funcion agregar tarea
+function agregarTarea(tarea,id,hecho,eliminar) {
+    if (eliminar) {
+        return
+    };
+    const realizado = hecho ? check : uncheck;
+    const LINE = hecho ? tachado : '';
+    const elemento = `<li class="li" id=elemento>
+                <span id="${id}" data="hecho"  class="material-symbols-outlined">${realizado}</span>
+               <p class="tareas-lista text ${LINE}">${tarea}</p>
+                <span id="${id}" data="eliminar" class="material-symbols-outlined">close</span></li>`
+            lista.insertAdjacentHTML ("beforeend",elemento);
+};
+
+function tareaRealizada(element) {
+        if (element.innerText === "radio_button_unchecked") {
+        element.innerText = "task_alt";
+    } else {
+        element.innerText ="radio_button_unchecked"
+    }
+
+    element.parentNode.querySelector('.text').classList.toggle(tachado);
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true;
+
+    
+};
+
+function tareaEliminada(element) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].eliminar =true;
+};
+
+botonAgregar.addEventListener("click", () => {
+    const tarea = input.value; 
+    if(tarea){
+        agregarTarea(tarea, id, false, false);
+        LIST.push({nombre: tarea, id: id, hecho:false, eliminar: false,});
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+        id++;
+        input.value = "";
+    }
+
 });
 
-typewriter.typeString('Desarrolladora Web Jr')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Soporte Tecnico y Community Manager')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>Conoce mas de mi.</strong>')
-    .pauseFor(2500)
-    .start();
+lista.addEventListener("click", function (event){
+    const element = event.target;
+    const elementData = element.attributes.data.value;
+    if (elementData =="hecho") {
+        tareaRealizada(element);
+        
+    } else if (elementData == "eliminar") {
+        tareaEliminada(element)};
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+
+});
+
+let data = localStorage.getItem ("TODO")
+if (data) {
+    LIST = JSON.parse(data);
+    id=LIST.lenght;
+    cargarLista(LIST);
+} else {
+    LIST =[];
+    id = 0;  
+}
+
+function cargarLista(array) {
+    array.forEach(
+        function (item){
+    agregarTarea(item.nombre, item.id, item.hecho, item.eliminar);
+        }
+    )
+};
